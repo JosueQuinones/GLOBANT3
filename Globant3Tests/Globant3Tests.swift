9605ac277c3fcdd5fee8fc0fd0623babf71dd2cb
em964 <ernesto.mejia@globant.com>
2019-06-26 15:58:44 -0500

    Add initial files

---
+++ Globant3Tests/Globant3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Globant3Tests.swift
+//  Globant3Tests
+//
+//  Created by Ernesto Daniel Mejia Valdiviezo on 6/26/19.
+//  Copyright © 2019 Ernesto Daniel Mejia Valdiviezo. All rights reserved.
+//
+
+import XCTest
+@testable import Globant3
+
+class Globant3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
